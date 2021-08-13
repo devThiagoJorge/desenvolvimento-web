3a5cc86f5827fc1b1a52861ab0a51b5a5a866b98
Thiago Vinicius Jorge <thiago.jorge@programmers.com.br>
2021-08-13 17:15:58 -0300

    first commit

---
+++ 01-FundamentoJS/modulo_3_variaveis.js
@@ -0,0 +1,31 @@
+// number 
+let number = 10
+let numberFloat = 10.00
+
+// string
+let string = "Eu sou uma string"
+
+// boolean
+let boolean = true; 
+
+// constante
+const constante = 10;
+
+// Desafio área do triângulo
+
+const PI = 3.141592;
+let raio = 10; 
+let areaQuadrado = PI * raio * raio;
+console.log(`A area do quadrado é: ${areaQuadrado}`);
+
+// Desafio troca de valores 
+let a = 7;
+let b = 94;
+
+let aux = b;
+
+b = a;
+a = aux;
+
+console.log("Valor de a: ", a);
+console.log("Valor de b: ", b);
